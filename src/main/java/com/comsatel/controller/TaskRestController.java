85255bd114bcbbe1facda13d71d907ce6933c5bc
ronaldfdg <ronaldfdg@gmail.com>
2020-07-18 11:13:14 -0500

    Function update done

--- src/main/java/com/comsatel/controller/TaskRestController.java
+++ src/main/java/com/comsatel/controller/TaskRestController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -30,10 +31,21 @@ public class TaskRestController {
 	}
 	
 	@PostMapping("/tasks")
+	@ResponseStatus(HttpStatus.CREATED)
 	public Task create(@RequestBody Task task) {
 		Task newTask = null;
 		newTask = serviceTask.save(task);
 		return newTask;
+	}
+	
+	@PutMapping("/tasks/{id}")
+	public Task update(@RequestBody Task task, @PathVariable("id") int id) {
+		Task currentTask = serviceTask.findById(id);
+		
+		currentTask.setDescription(task.getDescription());
+		currentTask.setPending(task.isPending());
+		
+		return serviceTask.save(currentTask);
 	}
 	
 	@DeleteMapping("/tasks/{id}")
